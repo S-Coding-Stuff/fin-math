c281244542a4a0d54694f069745c325a30f9ed89
S-Coding-Stuff <sam.grant.info@gmail.com>
2025-08-30 21:58:34 +0100

    Updated black_scholes.py - Added Volatility class to calculate historic and implied volatility

--- black_scholes.py
+++ black_scholes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.optimize
 from scipy.stats import norm
 
 
@@ -66,3 +67,49 @@ class Greeks(OptionPrice):
             return self.X * self.T * np.exp(-self.r * self.T) * norm.cdf(self.d2)
         else:
             return -self.X * self.T * np.exp(-self.r * self.T) * norm.cdf(-self.d2)
+
+
+class Volatility:
+
+    def __init__(self, S_0, r, trading_days):
+        self.S_0 = S_0
+        self.r = r
+        self.trading_days = trading_days
+
+    @staticmethod
+    def implied_volatility(self, market_price, S_0, X, r, T, call=True):
+        """
+        Solve for implied volatility using the Black–Scholes model.
+
+        args:
+            market_price: observed option price
+            S0: spot price
+            X: strike price
+            r: risk-free rate
+            T: time to maturity (in years)
+            call: True for call, False for put
+        """
+
+        def d1(sigma):
+            return (np.log(S_0 / X) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+        def d2(sigma):
+            return d1(sigma) - sigma * np.sqrt(T)
+
+        def blackscholes_price(sigma):
+            if call:
+                return S_0 * norm.cdf(d1(sigma)) - X * np.exp(-r * T) * norm.cdf(d2(sigma))
+            else:
+                return X * np.exp(-r * T) * norm.cdf(-d2(sigma)) - S_0 * np.exp(-r * T) * norm.cdf(-d1(sigma))
+
+        def objective(sigma):
+            return blackscholes_price(sigma) - market_price
+
+        try:
+            return scipy.optimize.brentq(objective, 1e-6, 5.0)  # Between 0.000001% and 500%
+        except ValueError:
+            return np.nan
+
+    @staticmethod
+    def historic_volatility(self, prices, trading_days=252):
+        return None
